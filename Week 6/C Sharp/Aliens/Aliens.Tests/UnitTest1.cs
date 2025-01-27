054cded8b28a5b6f27c9192c13718a36ca771c54
Thomas Lennon <187573206+thomas-lennon-atu-ie@users.noreply.github.com>
2025-01-27 19:36:39 +0000

    add Aliens.Tests empty start

---
+++ Week 6/C Sharp/Aliens/Aliens.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+// Written by Dr. Shane Wilson.
+// The author licenses this file to you under the MIT license.
+// See the LICENSE file in the solution root for more information.
+
+namespace Aliens.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
